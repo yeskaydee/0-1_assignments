0726107c5bdaf5917b1633f767fec04e1ef205d5
Santosh Kumar Doodala <84093797+yeskaydee@users.noreply.github.com>
2024-06-10 13:46:49 +0530

    fileServer

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -18,4 +18,44 @@ const path = require('path');
 const app = express();
 
 
+app.get('/files',function(req,res){
+  const directoryPath = path.join(__dirname, 'files');
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      res.status(500).send('Unable to access the directory');
+    }
+    else{
+      res.json(files);
+    }
+  });
+
+})
+app.get('/file/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'files', filename);
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        return res.status(404).send('File not found');
+      } else {
+        return res.status(500).send('Error reading file');
+      }
+    }
+
+    res.status(200).send(data);
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send('Route not found');
+});
+
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
 module.exports = app;
--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -104,9 +104,9 @@
     res.status(404).send('404 Not Found');
   });
 
-  // app.listen(port, function()  {
-  //   console.log(`Example app listening on port ${port}`)
-  // })
+  app.listen(port, function()  {
+    console.log(`Example app listening on port ${port}`)
+  })
 
   
   module.exports = app;

b7c4b1f23417833e635e3513ab67eab15f173319
Santosh Kumar Doodala <84093797+yeskaydee@users.noreply.github.com>
2024-06-12 18:19:38 +0530

    env variables

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -12,6 +12,7 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
+require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
